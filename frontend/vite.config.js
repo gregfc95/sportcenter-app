3b4fe9a4dc85ce09db49067ef59149935fb35c64
gregfc95 <josegfc95@gmail.com>
2026-05-13 15:38:38 -0300

    Actualizar CSS con V4 tailwind format

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,11 @@
 // frontend/vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
