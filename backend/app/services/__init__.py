61cfb014abc10be686629c06ad4dce4c3d3e41fc
gregfc95 <josegfc95@gmail.com>
2026-05-29 14:28:29 -0300

    Add random password generator

--- backend/app/services/__init__.py
+++ backend/app/services/__init__.py
@@ -3,6 +3,7 @@ from .actividad_service import ActividadService
 from .user_service import UserService
 from .reserva_service import ReservaService
 from .pago_service import PagoService
+from .email_service import send_password_email
 
 __all__ = [
     "TurnoService",
@@ -10,4 +11,5 @@ __all__ = [
     "UserService",
     "ReservaService",
     "PagoService",
+    "send_password_email",
 ]
