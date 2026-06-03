b4cb24c7b82023262402c3c506d4befb719cbd3b
gregfc95 <josegfc95@gmail.com>
2026-06-03 18:16:20 -0300

    Add pago and turnos routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import CrearTurnoPage from "./pages/CrearTurnoPage";
 import MisTurnosPage from "./pages/MisTurnosPage";
 import MisPagosPage from "./pages/MisPagosPage";
+import PagosPage from "./pages/PagosPage";
+import TurnosReservadosPage from "./pages/TurnosReservadosPage";
+import TurnoReservadoDetailPage from "./pages/TurnoReservadoDetailPage";
 import NuevaReservaPage from "./pages/NuevaReservaPage";
 import PagoResultadoPage from "./pages/PagoResultadoPage";
 import PagoCanceladoPage from "./pages/PagoCanceladoPage";
@@ -45,6 +48,12 @@ function App() {
             <Route path="/crear-turno" element={<CrearTurnoPage />} />
             <Route path="/mis-turnos" element={<MisTurnosPage />} />
             <Route path="/mis-pagos" element={<MisPagosPage />} />
+            <Route path="/pagos" element={<PagosPage />} />
+            <Route path="/turnos" element={<TurnosReservadosPage />} />
+            <Route
+              path="/turnos/:turnoId/:fecha"
+              element={<TurnoReservadoDetailPage />}
+            />
             <Route path="/nueva-reserva" element={<NuevaReservaPage />} />
             <Route path="/pago/exito" element={<PagoResultadoPage />} />
             <Route path="/pago/error" element={<PagoResultadoPage />} />
